1fd56c7e862fd0354b681b0f0a9405f339f5dca1
Daniel Böning <D.Boening@hotmail.de>
2020-05-21 18:34:07 +0200

    design weather

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,12 +1,67 @@
 import React from 'react';
 import './Weather.css';
 import Container from '../commons/Container/Container';
+import weatherIcon from '../../assets/icons/weather/cloudy.png'
+import iconWindspeed from '../../assets/icons/weather/wind.png'
+import iconRainProbability from '../../assets/icons/weather/rain.png'
 
-function Weather() {
+
+function renderForecastItem(weather) {
+  return (
+    <div className="WeatherForecastItem">
+      <img className="WeatherNowInfoImage" src={weatherIcon} />
+      <div className="WeatherForecastLabel">5 &deg; C</div>
+      <div className="WeatherForecastLabel">Montag</div>
+    </div>
+  )
+}
+
+function renderForecast() {
+  return (
+    <div className="WeatherForecastContainer">
+      {renderForecastItem()}
+      {renderForecastItem()}
+      {renderForecastItem()}
+    </div>
+  );
+}
+
+function renderWeatherNow() {
+  return (
+    <div className="WeatherNowContainer">
+              <div>
+                <img style={{ width: 128, height:128 }}  src={weatherIcon}/>
+                <div className="WeatherTempNowLabel" >5 &deg; C</div>
+              </div>
+
+            <div className="WeatherNowInfoContainer" layout="column">
+              <div className="WeatherNowInfoItem" >
+                <img className="WeatherNowInfoImage"  src={iconWindspeed}/>
+                <div className="WeatherNowInfoLabel">7 km/h</div>    
+              </div>
+
+              <div className="WeatherNowInfoItem">
+                <img className="WeatherNowInfoImage"  src={iconRainProbability}/>
+                <div className="WeatherNowInfoLabel">0 mm</div>    
+              </div> 
+            </div>
+        </div>
+  )
+}
+
+function Weather(props) {
   return (
     <Container>
         <div className="Weather">
-        WETTER123
+          <div className="WeatherMainContainer">
+            <div style={{fontSize: 64, fontWeight: 400}}>14:20</div>
+            <h3>Donnerstag, 21.05.</h3> 
+            <div style={{ paddingTop: 20}}>
+              {renderWeatherNow()}
+            </div>
+          </div>
+          <div className="Seperator"/>
+          {renderForecast()}
         </div>
     </Container>  
   );
