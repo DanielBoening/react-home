193fc7f12c2cf67da35c88b6087e5be3dad7e29d
Daniel Böning <D.Boening@hotmail.de>
2020-05-26 17:34:12 +0200

    add alerts

--- src/components/ToDo/ToDo.js
+++ src/components/ToDo/ToDo.js
@@ -4,6 +4,7 @@ import Container from '../commons/Container/Container';
 import Header from '../commons/Header/Header';
 import {ReactComponent as TodoIcon} from '../../assets/icons/todo/playlist-check.svg';
 import {ReactComponent as PlusIcon} from '../../assets/icons/plus.svg';
+import { useAlert } from 'react-alert'
 
 function ToDo(props) {
     const [todos, setTodos] = useState([
@@ -116,22 +117,24 @@ function ToDo(props) {
         {title: 'Putzen', active: false}, 
         {title: 'Waschen', active: true},
     ]);
+    function showNewToDoDialog() {
+    }
 
-  return (
-    <Container>
-        <div className="ToDo">
-            <Header 
-                title={'ToDo'} 
-                iconDisabled={() => TodoIcon}
-                iconAction={() => PlusIcon}
-                action={() => console.log('add todo')}
-                />
-            <ul className="ToDoList">
-                {todos.map(t => <li className="ToDoListItem">{t.title}</li>)}
-            </ul>
-        </div>
-    </Container>  
-  );
+    return (
+        <Container>
+            <div className="ToDo">
+                <Header 
+                    title={'ToDo'} 
+                    iconDisabled={() => TodoIcon}
+                    iconAction={() => PlusIcon}
+                    action={showNewToDoDialog}
+                    />
+                <ul className="ToDoList">
+                    {todos.map(t => <li className="ToDoListItem">{t.title}</li>)}
+                </ul>
+            </div>
+        </Container>  
+    );
 }
 
 export default ToDo;
